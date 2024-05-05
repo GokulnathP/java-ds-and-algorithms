a040cde732ad0f17dcd85ed8ec98c72f2271cb09
GokulnathP <4gokul2000@gmail.com>
2024-05-05 10:48:02 +0530

    [Gokul] DS Recursion in binary search tree

--- src/main/java/com/gokulnathp/datastractures/BinarySearchTree.java
+++ src/main/java/com/gokulnathp/datastractures/BinarySearchTree.java
@@ -1,23 +1,28 @@
 package com.gokulnathp.datastractures;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-
 public class BinarySearchTree {
-    private TreeNode root = null;
+    public static class Node {
+        int value;
+        Node left;
+        Node right;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    private Node root = null;
 
     public boolean insert(int value) {
-        TreeNode newNode = new TreeNode(value);
+        Node newNode = new Node(value);
 
         if (root == null) {
             root = newNode;
             return true;
         }
 
-        TreeNode currentNode = root;
-        TreeNode prevNode = null;
+        Node currentNode = root;
+        Node prevNode = null;
 
         while (currentNode != null) {
             if (currentNode.value == value) return false;
@@ -34,8 +39,24 @@ public class BinarySearchTree {
         return true;
     }
 
+    private Node rInsert(Node currentNode, int value) {
+        if (currentNode == null) return new Node(value);
+
+        if (value < currentNode.value) {
+            currentNode.left = rInsert(currentNode.left, value);
+        } else if (value > currentNode.value) {
+            currentNode.right = rInsert(currentNode.right, value);
+        }
+
+        return currentNode;
+    }
+
+    public void rInsert(int value) {
+        root = rInsert(root, value);
+    }
+
     public boolean contains(int value) {
-        TreeNode currentNode = root;
+        Node currentNode = root;
 
         while (currentNode != null) {
             if (currentNode.value == value) return true;
@@ -47,7 +68,75 @@ public class BinarySearchTree {
         return false;
     }
 
-    public TreeNode getRoot() {
+    private boolean rContains(Node currentNode, int value) {
+        if (currentNode == null) return false;
+
+        if (currentNode.value == value) return true;
+
+        if (value < currentNode.value) return rContains(currentNode.left, value);
+
+        return rContains(currentNode.right, value);
+    }
+
+    public boolean rContains(int value) {
+        return rContains(root, value);
+    }
+
+    public int minValue(Node currentNode) {
+        while (currentNode.left != null) currentNode = currentNode.left;
+        return currentNode.value;
+    }
+
+    private Node deleteNode(Node currentNode, int value) {
+        if (currentNode == null) return null;
+
+        if (value < currentNode.value) currentNode.left = deleteNode(currentNode.left, value);
+        else if (value > currentNode.value) currentNode.right = deleteNode(currentNode.right, value);
+        else {
+            if (currentNode.left == null && currentNode.right == null) return null;
+            if (currentNode.left == null) return currentNode.right;
+            if (currentNode.right == null) return currentNode.left;
+
+            currentNode.value = minValue(currentNode.right);
+            currentNode.right = deleteNode(currentNode.right, currentNode.value);
+        }
+
+        return currentNode;
+    }
+
+    public void deleteNode(int value) {
+        root = deleteNode(root, value);
+    }
+
+    private Node sortedArrayToBST(int[] nums, int left, int right) {
+        if(right < left) return null;
+
+        int mid = left + (right - left) / 2;
+        Node node = new Node(nums[mid]);
+        node.left = sortedArrayToBST(nums, left, mid - 1);
+        node.right = sortedArrayToBST(nums, mid + 1, right);
+        return node;
+    }
+
+    public void sortedArrayToBST(int[] nums) {
+        this.root = sortedArrayToBST(nums, 0, nums.length - 1);
+    }
+
+    private Node invertTree(Node currentNode) {
+        if(currentNode == null) return null;
+
+        Node leftNode = currentNode.left;
+        currentNode.left = invertTree(currentNode.right);
+        currentNode.right = invertTree(leftNode);
+
+        return currentNode;
+    }
+
+    public void invertTree() {
+        root = invertTree(root);
+    }
+
+    public Node getRoot() {
         return root;
     }
 }
--- src/test/java/com/gokulnathp/datastractures/BinarySearchTreeTest.java
+++ src/test/java/com/gokulnathp/datastractures/BinarySearchTreeTest.java
@@ -44,6 +44,46 @@ class BinarySearchTreeTest {
     }
 
     @Nested
+    class RInsert {
+        @Test
+        void shouldSetRootNodeToEmptyTree() {
+            BinarySearchTree binarySearchTree = new BinarySearchTree();
+
+            binarySearchTree.rInsert(2);
+
+            assertEquals(2, binarySearchTree.getRoot().value);
+        }
+
+        @Test
+        void shouldInsertNodeToLeftWhenValueIsLow() {
+            BinarySearchTree binarySearchTree = new BinarySearchTree();
+            binarySearchTree.rInsert(2);
+
+            binarySearchTree.rInsert(1);
+
+            assertEquals(1, binarySearchTree.getRoot().left.value);
+        }
+
+        @Test
+        void shouldInsertNodeToRightWhenValueIsHigh() {
+            BinarySearchTree binarySearchTree = new BinarySearchTree();
+            binarySearchTree.rInsert(2);
+
+            binarySearchTree.rInsert(3);
+
+            assertEquals(3, binarySearchTree.getRoot().right.value);
+        }
+
+        @Test
+        void shouldReturnFalseIfTheValueAlreadyExist() {
+            BinarySearchTree binarySearchTree = new BinarySearchTree();
+            binarySearchTree.rInsert(1);
+
+            binarySearchTree.rInsert(1);
+        }
+    }
+
+    @Nested
     class Contains {
         @Test
         void shouldReturnFalseIfTreeIsEmpty() {
@@ -90,6 +130,328 @@ class BinarySearchTreeTest {
             binarySearchTree.insert(56);
 
             assertFalse(binarySearchTree.contains(72));
+        }
+    }
+
+    @Nested
+    class RContains {
+        @Test
+        void shouldReturnFalseIfTreeIsEmpty() {
+            BinarySearchTree binarySearchTree = new BinarySearchTree();
+
+            assertFalse(binarySearchTree.rContains(1));
+        }
+
+        @Test
+        void shouldReturnFalseForTreeWithOneNode() {
+            BinarySearchTree binarySearchTree = new BinarySearchTree();
+            binarySearchTree.insert(2);
+
+            assertFalse(binarySearchTree.rContains(1));
+        }
+
+        @Test
+        void shouldReturnTrueForTreeWithOneNode() {
+            BinarySearchTree binarySearchTree = new BinarySearchTree();
+            binarySearchTree.insert(2);
+
+            assertTrue(binarySearchTree.rContains(2));
+        }
+
+        @Test
+        void shouldReturnTrueIfTreeHasTheValue() {
+            BinarySearchTree binarySearchTree = new BinarySearchTree();
+            binarySearchTree.insert(2);
+            binarySearchTree.insert(48);
+            binarySearchTree.insert(27);
+            binarySearchTree.insert(71);
+            binarySearchTree.insert(56);
+
+            assertTrue(binarySearchTree.rContains(27));
+        }
+
+        @Test
+        void shouldReturnFalseWhenTheValueIsNotPresent() {
+            BinarySearchTree binarySearchTree = new BinarySearchTree();
+            binarySearchTree.insert(2);
+            binarySearchTree.insert(48);
+            binarySearchTree.insert(27);
+            binarySearchTree.insert(71);
+            binarySearchTree.insert(56);
+
+            assertFalse(binarySearchTree.rContains(72));
+        }
+    }
+
+    @Nested
+    class MinValue {
+        @Test
+        void shouldReturnRootWhenTreeHasOnlyOneNode() {
+            BinarySearchTree binarySearchTree = new BinarySearchTree();
+            binarySearchTree.rInsert(2);
+
+            assertEquals(2, binarySearchTree.minValue(binarySearchTree.getRoot()));
+        }
+
+        @Test
+        void shouldReturnLeftValueWhenTreeHasOneNodeLayer() {
+            BinarySearchTree binarySearchTree = new BinarySearchTree();
+            binarySearchTree.rInsert(2);
+            binarySearchTree.rInsert(3);
+            binarySearchTree.rInsert(1);
+
+            assertEquals(1, binarySearchTree.minValue(binarySearchTree.getRoot()));
+        }
+
+        @Test
+        void shouldReturnLeftMostValue() {
+            BinarySearchTree binarySearchTree = new BinarySearchTree();
+            binarySearchTree.rInsert(5);
+            binarySearchTree.rInsert(2);
+            binarySearchTree.rInsert(1);
+            binarySearchTree.rInsert(3);
+
+            assertEquals(1, binarySearchTree.minValue(binarySearchTree.getRoot()));
+        }
+    }
+
+    @Nested
+    class DeleteNode {
+        @Test
+        void shouldRemoveRootIfTreeHasOnlyOneNode() {
+            BinarySearchTree binarySearchTree = new BinarySearchTree();
+            binarySearchTree.rInsert(1);
+
+            binarySearchTree.deleteNode(1);
+
+            assertNull(binarySearchTree.getRoot());
+        }
+
+        @Test
+        void shouldRemoveNodeWithNoChild() {
+            BinarySearchTree binarySearchTree = new BinarySearchTree();
+            binarySearchTree.rInsert(2);
+            binarySearchTree.rInsert(1);
+            binarySearchTree.rInsert(3);
+
+            binarySearchTree.deleteNode(1);
+
+            assertEquals(2, binarySearchTree.getRoot().value);
+            assertEquals(3, binarySearchTree.getRoot().right.value);
+            assertNull(binarySearchTree.getRoot().left);
+        }
+
+        @Test
+        void shouldRemoveNodeWithLeftChild() {
+            BinarySearchTree binarySearchTree = new BinarySearchTree();
+            binarySearchTree.rInsert(5);
+            binarySearchTree.rInsert(1);
+            binarySearchTree.rInsert(3);
+            binarySearchTree.rInsert(2);
+
+            binarySearchTree.deleteNode(3);
+
+            assertEquals(2, binarySearchTree.getRoot().left.right.value);
+        }
+
+        @Test
+        void shouldRemoveNodeWithRightChild() {
+            BinarySearchTree binarySearchTree = new BinarySearchTree();
+            binarySearchTree.rInsert(5);
+            binarySearchTree.rInsert(1);
+            binarySearchTree.rInsert(3);
+            binarySearchTree.rInsert(4);
+
+            binarySearchTree.deleteNode(3);
+
+            assertEquals(4, binarySearchTree.getRoot().left.right.value);
+        }
+
+        @Test
+        void shouldRemoveNodeWithBothChild() {
+            BinarySearchTree binarySearchTree = new BinarySearchTree();
+            binarySearchTree.rInsert(5);
+            binarySearchTree.rInsert(1);
+            binarySearchTree.rInsert(3);
+            binarySearchTree.rInsert(6);
+            binarySearchTree.rInsert(2);
+            binarySearchTree.rInsert(4);
+
+            binarySearchTree.deleteNode(3);
+
+            assertEquals(4, binarySearchTree.getRoot().left.right.value);
+        }
+    }
+
+    @Nested
+    class SortedArrayToBST {
+        @Test
+        void testEmptyArray() {
+            BinarySearchTree binarySearchTree = new BinarySearchTree();
+
+            binarySearchTree.sortedArrayToBST(new int[]{});
+
+            assertNull(binarySearchTree.getRoot());
+        }
+
+        @Test
+        void testArrayWithOneElement() {
+            BinarySearchTree binarySearchTree = new BinarySearchTree();
+
+            binarySearchTree.sortedArrayToBST(new int[]{1});
+
+            assertEquals(1, binarySearchTree.getRoot().value);
+            assertNull(binarySearchTree.getRoot().left);
+            assertNull(binarySearchTree.getRoot().right);
+        }
+
+        @Test
+        void testArrayWithTwoElements() {
+            BinarySearchTree binarySearchTree = new BinarySearchTree();
+
+            binarySearchTree.sortedArrayToBST(new int[]{1, 2});
+
+            assertEquals(1, binarySearchTree.getRoot().value);
+            assertNull(binarySearchTree.getRoot().left);
+            assertEquals(2, binarySearchTree.getRoot().right.value);
+        }
+
+        @Test
+        void testArrayWithThreeElements() {
+            BinarySearchTree binarySearchTree = new BinarySearchTree();
+
+            binarySearchTree.sortedArrayToBST(new int[]{1, 2, 3});
+
+            assertEquals(2, binarySearchTree.getRoot().value);
+            assertEquals(1, binarySearchTree.getRoot().left.value);
+            assertEquals(3, binarySearchTree.getRoot().right.value);
+        }
+
+        @Test
+        void testArrayWithFourElements() {
+            BinarySearchTree binarySearchTree = new BinarySearchTree();
+
+            binarySearchTree.sortedArrayToBST(new int[]{1, 2, 3, 4});
+
+            assertEquals(2, binarySearchTree.getRoot().value);
+            assertEquals(1, binarySearchTree.getRoot().left.value);
+            assertEquals(3, binarySearchTree.getRoot().right.value);
+            assertEquals(4, binarySearchTree.getRoot().right.right.value);
+        }
+
+        @Test
+        void testArrayWithOddNumberOfElements() {
+            BinarySearchTree binarySearchTree = new BinarySearchTree();
+
+            binarySearchTree.sortedArrayToBST(new int[]{1, 2, 3, 4, 5});
+
+            assertEquals(3, binarySearchTree.getRoot().value);
+            assertEquals(1, binarySearchTree.getRoot().left.value);
+            assertEquals(2, binarySearchTree.getRoot().left.right.value);
+            assertEquals(4, binarySearchTree.getRoot().right.value);
+            assertEquals(5, binarySearchTree.getRoot().right.right.value);
+        }
+    }
+
+    @Nested
+    class InvertTree {
+        @Test
+        void testEmptyTree() {
+            BinarySearchTree binarySearchTree = new BinarySearchTree();
+
+            binarySearchTree.invertTree();
+
+            assertNull(binarySearchTree.getRoot());
+        }
+
+        @Test
+        void testTreeWithOneNode() {
+            BinarySearchTree binarySearchTree = new BinarySearchTree();
+            binarySearchTree.rInsert(2);
+
+            binarySearchTree.invertTree();
+
+            assertEquals(2, binarySearchTree.getRoot().value);
+        }
+
+        @Test
+        void testTreeWithTwoNode() {
+            BinarySearchTree binarySearchTree = new BinarySearchTree();
+            binarySearchTree.rInsert(2);
+            binarySearchTree.rInsert(1);
+
+            binarySearchTree.invertTree();
+
+            assertEquals(2, binarySearchTree.getRoot().value);
+            assertEquals(1, binarySearchTree.getRoot().right.value);
+        }
+
+        @Test
+        void testTreeWithThreeNode() {
+            BinarySearchTree binarySearchTree = new BinarySearchTree();
+            binarySearchTree.rInsert(2);
+            binarySearchTree.rInsert(1);
+            binarySearchTree.rInsert(3);
+
+            binarySearchTree.invertTree();
+
+            assertEquals(2, binarySearchTree.getRoot().value);
+            assertEquals(3, binarySearchTree.getRoot().left.value);
+            assertEquals(1, binarySearchTree.getRoot().right.value);
+        }
+
+        @Test
+        void testTreeWithFourNode() {
+            BinarySearchTree binarySearchTree = new BinarySearchTree();
+            binarySearchTree.rInsert(2);
+            binarySearchTree.rInsert(1);
+            binarySearchTree.rInsert(3);
+            binarySearchTree.rInsert(4);
+
+            binarySearchTree.invertTree();
+
+            assertEquals(2, binarySearchTree.getRoot().value);
+            assertEquals(3, binarySearchTree.getRoot().left.value);
+            assertEquals(4, binarySearchTree.getRoot().left.left.value);
+            assertEquals(1, binarySearchTree.getRoot().right.value);
+        }
+
+        @Test
+        void testTreeWithFiveNode() {
+            BinarySearchTree binarySearchTree = new BinarySearchTree();
+            binarySearchTree.rInsert(3);
+            binarySearchTree.rInsert(1);
+            binarySearchTree.rInsert(2);
+            binarySearchTree.rInsert(4);
+            binarySearchTree.rInsert(5);
+
+            binarySearchTree.invertTree();
+
+            assertEquals(3, binarySearchTree.getRoot().value);
+            assertEquals(4, binarySearchTree.getRoot().left.value);
+            assertEquals(5, binarySearchTree.getRoot().left.left.value);
+            assertEquals(1, binarySearchTree.getRoot().right.value);
+            assertEquals(2, binarySearchTree.getRoot().right.left.value);
+        }
+
+        @Test
+        void testTreeWithSixNode() {
+            BinarySearchTree binarySearchTree = new BinarySearchTree();
+            binarySearchTree.rInsert(3);
+            binarySearchTree.rInsert(1);
+            binarySearchTree.rInsert(2);
+            binarySearchTree.rInsert(5);
+            binarySearchTree.rInsert(4);
+            binarySearchTree.rInsert(6);
+
+            binarySearchTree.invertTree();
+
+            assertEquals(3, binarySearchTree.getRoot().value);
+            assertEquals(5, binarySearchTree.getRoot().left.value);
+            assertEquals(6, binarySearchTree.getRoot().left.left.value);
+            assertEquals(4, binarySearchTree.getRoot().left.right.value);
+            assertEquals(1, binarySearchTree.getRoot().right.value);
+            assertEquals(2, binarySearchTree.getRoot().right.left.value);
         }
     }
 }
